d169bfe814fa7c6f8f313cb884845216495a820a
vishuu12 <93487507+vishuu12@users.noreply.github.com>
2021-10-31 23:37:43 +0530

    dude

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log('manish kumar');
 console.log('new branch from manish');
 console.log('new commit');
-console.log('another one from hero')
+console.log('another one from hero and dude')
 console.log('i am hero')
 console.log('this one from villan');
 
